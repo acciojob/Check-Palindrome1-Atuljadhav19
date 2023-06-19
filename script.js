e39aa02c76d901877c3b684e238492f1104fa3df
Atuljadhav19 <133120719+Atuljadhav19@users.noreply.github.com>
2023-06-19 02:27:57 -0700

    created a submission

--- script.js
+++ script.js
@@ -1,19 +1,17 @@
 // complete the given function
 
-function palindrome(str){
-	let start = str[0];
-	let end = str.length;
-	for(let i = 0; i < str.length; i++)
-		{
-			if(start != end)
-			{
-				return true;
-			}
-			else{
-				return false;
-			}
-			start++;
-			end++;
-		}
-}
+function palindrome(str) {
+  let start = 0;
+  let end = str.length - 1;
+  
+  while (start < end) {
+    if (str[start] !== str[end]) {
+      return false; // Characters don't match, not a palindrome
+    }
+    
+    start++;
+    end--;
+  }
+  
+  return true;
 module.exports = palindrome
